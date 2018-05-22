90d23c78fb954052017a1cc61ba430506dba15dc
Germain Haugou <germain.haugou@iie.ee.ethz.ch>
2018-05-22 13:54:06 +0200

    Switched vega to soc_eu v2

--- include/hal/soc_eu/soc_eu_v2.h
+++ include/hal/soc_eu/soc_eu_v2.h
@@ -14,11 +14,11 @@
  * limitations under the License.
  */
 
-#ifndef __HAL_SOC_EU_SOC_EU_V1_H__
-#define __HAL_SOC_EU_SOC_EU_V1_H__
+#ifndef __HAL_SOC_EU_SOC_EU_V2_H__
+#define __HAL_SOC_EU_SOC_EU_V2_H__
 
 #include "archi/pulp.h"
-#include "archi/soc_eu/soc_eu_v1.h"
+#include "archi/soc_eu/soc_eu_v2.h"
 
 static inline void soc_eu_eventMask_set(unsigned int reg, unsigned int value) {
   pulp_write32(ARCHI_SOC_EU_ADDR + reg, value);
@@ -35,50 +35,38 @@ static inline void soc_eu_eventMask_reset(unsigned int first_reg) {
   }
 }
 
-static inline void soc_eu_eventMask_setEvent(int evt, unsigned int lsbReg, unsigned int msbReg) {
-  unsigned int reg;
-  if (evt >= 32) {
-    reg = msbReg;
-    evt -= 32;
-  } else {
-    reg = lsbReg;
-  }
+static inline void soc_eu_eventMask_setEvent(int evt, unsigned int first_reg) {
+  unsigned int reg = first_reg + (evt / 32 * 4);
   soc_eu_eventMask_set(reg, soc_eu_eventMask_get(reg) & ~(1 << evt));
 }
 
-static inline void soc_eu_eventMask_clearEvent(int evt, unsigned int lsbReg, unsigned int msbReg) {
-  unsigned int reg;
-  if (evt >= 32) {
-    reg = msbReg;
-    evt -= 32;
-  } else {
-    reg = lsbReg;
-  }
+static inline void soc_eu_eventMask_clearEvent(int evt, unsigned int first_reg) {
+  unsigned int reg = first_reg + (evt / 32 * 4);
   soc_eu_eventMask_set(reg, soc_eu_eventMask_get(reg) | (1 << evt));
 }
 
 static inline void soc_eu_fcEventMask_setEvent(int evt) {
-  soc_eu_eventMask_setEvent(evt, SOC_FC_MASK_LSB, SOC_FC_MASK_MSB);
+  soc_eu_eventMask_setEvent(evt, SOC_FC_FIRST_MASK);
 }
 
 static inline void soc_eu_prEventMask_setEvent(int evt) {
-  soc_eu_eventMask_setEvent(evt, SOC_PR_MASK_LSB, SOC_PR_MASK_MSB);
+  soc_eu_eventMask_setEvent(evt, SOC_PR_FIRST_MASK);
 }
 
 static inline void soc_eu_clEventMask_setEvent(int clusterId, int evt) {
-  soc_eu_eventMask_setEvent(evt, SOC_CL_MASK_LSB, SOC_CL_MASK_MSB);
+  soc_eu_eventMask_setEvent(evt, SOC_CL_FIRST_MASK);
 }
 
 static inline void soc_eu_fcEventMask_clearEvent(int evt) {
-  soc_eu_eventMask_clearEvent(evt, SOC_FC_MASK_LSB, SOC_FC_MASK_MSB);
+  soc_eu_eventMask_clearEvent(evt, SOC_FC_FIRST_MASK);
 }
 
 static inline void soc_eu_prEventMask_clearEvent(int evt) {
-  soc_eu_eventMask_clearEvent(evt, SOC_PR_MASK_LSB, SOC_PR_MASK_MSB);
+  soc_eu_eventMask_clearEvent(evt, SOC_PR_FIRST_MASK);
 }
 
 static inline void soc_eu_clEventMask_clearEvent(int clusterId, int evt) {
-  soc_eu_eventMask_clearEvent(evt, SOC_CL_MASK_LSB, SOC_CL_MASK_MSB);
+  soc_eu_eventMask_clearEvent(evt, SOC_CL_FIRST_MASK);
 }
 
 
