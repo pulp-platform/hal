dc714a1d5b2ffb5b637817eb9f62c54ab86aa07e
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2019-04-01 15:25:47 +0200

    Added new soc_eu_v3 for vivosoc3_1

--- include/hal/chips/vivosoc3_1/pulp.h
+++ include/hal/chips/vivosoc3_1/pulp.h
@@ -22,7 +22,7 @@
 #include "hal/itc/itc_v1.h"
 #include "hal/dma/mchan_v6.h"
 #include "hal/timer/timer_v2.h"
-#include "hal/soc_eu/soc_eu_v2.h"
+#include "hal/soc_eu/soc_eu_v3.h"
 #include "hal/udma/udma_v3.h"
 #include "hal/cluster_ctrl/cluster_ctrl_v2.h"
 #include "hal/icache/icache_ctrl_v2.h"
