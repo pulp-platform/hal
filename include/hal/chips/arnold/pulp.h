a882caf5efd59496b1d5ab77e8bb560b461134dd
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2019-07-01 17:46:38 +0200

    Switched arnold to gpio v3

--- include/hal/chips/arnold/pulp.h
+++ include/hal/chips/arnold/pulp.h
@@ -26,7 +26,7 @@
 #include "hal/rom/rom_v2.h"
 #include "hal/fll/fll_v1.h"
 #include "archi/stdout/stdout_v3.h"
-#include "hal/gpio/gpio_v2.h"
+#include "hal/gpio/gpio_v3.h"
 
 #include "hal/udma/cpi/udma_cpi_v1.h"
 #include "hal/udma/i2c/udma_i2c_v2.h"
