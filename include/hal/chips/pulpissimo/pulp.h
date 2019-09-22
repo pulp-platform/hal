0895d6d7f7c5befc38ae5aed7f4e168349685cca
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2019-09-22 07:53:30 +0200

    Fixed regression

--- include/hal/chips/pulpissimo/pulp.h
+++ include/hal/chips/pulpissimo/pulp.h
@@ -30,7 +30,6 @@
 #include "hal/udma/udma_v3.h"
 #include "hal/udma/cpi/udma_cpi_v1.h"
 #include "hal/udma/i2c/udma_i2c_v2.h"
-#include "hal/udma/i2s/udma_i2s_v2.h"
 #include "hal/udma/spim/udma_spim_v3.h"
 #include "hal/udma/uart/udma_uart_v1.h"
 
